8f272a9af6717ad0d42299ab783418fd2c47be54
Gaden <gade1201@gmail.com>
2017-07-15 14:38:54 +0200

    +Player controller

---
+++ Assets/Player_Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour {
+
+    public float MovSpeed;
+
+    public float torque;
+    public Rigidbody2D body;
+
+
+    void Update () {
+        print(Input.GetButton("p1_right"));
+
+        if(Input.GetButton("p1_right") == true && Input.GetButton("p1_left") == false)
+        {
+            body.velocity = new Vector2(0, 0);
+            body.angularVelocity = -100;
+        }
+
+
+        else if(Input.GetButton("p1_left") == true && Input.GetButton("p1_right") == false)
+        {
+            body.velocity = new Vector2(0, 0);
+            body.angularVelocity = 100;
+        }
+
+
+        else if(Input.GetButton("p1_left") == true && Input.GetButton("p1_right") == true)
+        {
+            body.angularVelocity = 0;
+            body.velocity = transform.up * MovSpeed;
+        }
+
+
+        else
+        {
+            body.velocity = new Vector2(0, 0);
+            body.angularVelocity = 0;
+        }
+    }
+}
